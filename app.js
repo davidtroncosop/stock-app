817ee99c49525ef0bc60c7ca057bfc7e7cef4838
David Troncoso <dtroncoso@miuandes.cl>
2023-03-05 16:26:09 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('¡Hola, mundo!');
+});
+
+app.listen(4000, () => {
+  console.log('Servidor iniciado en el puerto 4000.');
+});
+
