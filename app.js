ce0be8025166302fc2eec18b47a1f9f63daaf7b9
David Troncoso <dtroncoso@miuandes.cl>
2023-03-19 17:25:23 -0300

    Cambiamos la variable de entorno PORT

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ app.get('/', (req, res) => {
   res.send('¡Hola, mundo!');
 });
 
+const PORT = process.env.PORT||4000
+
 app.listen(4000, () => {
   console.log('Servidor iniciado en el puerto 4000.');
 });
